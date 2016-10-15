f0e7d7687d4681a00bd8423acc90b423c003b6d1
skydiverb <bryanbuitrago86@gmail.com>
2016-10-14 20:03:36 -0400

    added logic to the card game on main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = 'king';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'queen';
+
+if(cardOne === cardThree || cardTwo === cardFour){
+    alert('You found a match!');
+}
+else {
+    alert('Sorry, try again.')
+}

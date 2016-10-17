9adb6667dd2c8dfd0e0b27c269ec2bdbea7acf87
skydiverb <bryanbuitrago86@gmail.com>
2016-10-16 22:18:47 -0400

    finished the functionality of the board, added an img css rule, and edited index.html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,45 @@
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPLay = [];
 
 var board = document.getElementById('game-board');
 
-var createCards = function(){
-    for(var i = 0; i < 4; i ++){
+var createBoard = function(){
+    for(var i = 0; i < cards.length; i ++){
         var cardElement = document.createElement('div');
         cardElement.className = 'card';
         board.appendChild(cardElement);
-        }
+        cardElement.setAttribute('data-card', cards[i]);
+        cardElement.addEventListener('click', isTwoCards);
+        
+        board.appendChild(cardElement);
+        board.appendChild(cardElement);
+        
+    }
+};
+
+function isTwoCards(){
+   console.log(this.getAttribute('data-card'));
+    	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; 
+	}cardsInPLay.push(this.getAttribute('data-card'));
+    if(cardsInPLay.length === 2){
+        isMatch(cardsInPLay);
+        cardsInPLay = [];
+    }
 }
-createCards();
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
 
